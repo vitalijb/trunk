0d4902099a848a0948afa7bf32596669091d4c98
vitalijb <vitalij.balandin@gmail.com>
2014-06-29 13:21:48 +0300

    commit

---
+++ eclipse_workspace/Hexlet_course/src/com/hexlet/buildings/Building.java
@@ -0,0 +1,5 @@
+package com.hexlet.buildings;
+public class Building {
+	public int roomsCount = 12;
+	public String address;
+}
---
+++ eclipse_workspace/Test/src/E04_DataOnly.java
@@ -0,0 +1,12 @@
+public class E04_DataOnly {
+ int i;
+ double d;
+ boolean b;
+ public static void main(String[] args) {
+  E04_DataOnly exc4 = new E04_DataOnly();
+  exc4.i = 2;
+  exc4.d = 2;
+  exc4.b = true;
+  System.out.println(exc4.i);
+ }
+}
